e8871dfdebd263103075997727b824385e19b7b0
Carlton Joseph <carlton.joseph@gmail.com>
2024-01-17 11:24:19 -0800

    Add api route protection

--- src/app/create-post/actions.ts
+++ src/app/create-post/actions.ts
@@ -3,10 +3,15 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+import { auth } from "@/auth";
 import { db } from "@/db";
 import { posts as postsTable } from "@/db/schema/posts";
 
 export async function createPost(content: string, id: string) {
+  const session = await auth();
+  if (!session?.user) {
+    return { message: "not authenticated" };
+  }
   if (!content || content.length < 3) {
     return { error: "not enough content" };
   }
